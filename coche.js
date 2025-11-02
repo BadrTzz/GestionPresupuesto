837b18f83af06a08934268cd4d0c57be062c4129
BadrTzz <tazichoto@gmail.com>
2025-11-02 19:24:10 +0100

    Finalizacion de la web basica de gestion de gastos

--- coche.js
+++
@@ -1,7 +0,0 @@
-let coches = [
-  {id: 1, marca: "Seat", modelo: "Ibiza", precio: 10000},
-  {id: 2, marca: "Seat", modelo: "Ateca", precio: 18000},
-  {id: 3, marca: "Volkswagen", modelo: "Golf", precio: 21000},
-  {id: 4, marca: "Kia", modelo: "Niro", precio: 30000}
-];
-
